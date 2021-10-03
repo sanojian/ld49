8a764fde6dbca51a82b52b2cfacd8a6052be2402
Jonas Olmstead <sanojian@gmail.com>
2021-10-03 15:01:40 +0200

    reactions

--- src/js/scenes/BootScene.js
+++ src/js/scenes/BootScene.js
@@ -13,8 +13,11 @@ class BootScene extends Phaser.Scene {
 
       this.load.image('chem1_source', '/dist/assets/gfx/chem1.png');
       this.load.image('chem2_source', '/dist/assets/gfx/chem2.png');
+      this.load.image('chem3_source', '/dist/assets/gfx/chem3.png');
+      this.load.image('chem4_source', '/dist/assets/gfx/chem4.png');
 
       this.load.audio('music1', '/dist/assets/audio/Chemical_track1.mp3');
+      this.load.audio('music2', '/dist/assets/audio/Chemical_track2.mp3');
 
     }
 
--- src/js/scenes/PlayScene.js
+++ src/js/scenes/PlayScene.js
@@ -13,14 +13,18 @@ class PlayScene extends Phaser.Scene {
 		let world = this.initWorld();
 
 		var graphics = this.add.graphics({
-			x: this.sys.game.scale.gameSize.width/2 - world.height*world.cellSize/2,
+			x: this.sys.game.scale.gameSize.width/2,
 			y: this.sys.game.scale.gameSize.height/2
 		});
 
 		let chem1ButtonDown = false;
-		let chem1Button = this.add.image(this.sys.game.scale.gameSize.width/2 - 8, graphics.y - 8, 'chem1_source');
+		let chem1Button = this.add.image(graphics.x + 8, graphics.y - 8, 'chem1_source');
 		let chem2ButtonDown = false;
-		let chem2Button = this.add.image(this.sys.game.scale.gameSize.width/2 + 8, graphics.y - 8, 'chem2_source');
+		let chem2Button = this.add.image(graphics.x + 24, graphics.y - 8, 'chem2_source');
+		let chem3ButtonDown = false;
+		let chem3Button = this.add.image(graphics.x + 40, graphics.y - 8, 'chem3_source');
+		let chem4ButtonDown = false;
+		let chem4Button = this.add.image(graphics.x + 56, graphics.y - 8, 'chem4_source').setVisible(false);
 
 
 		chem1Button.setInteractive({ useHandCursor: true});
@@ -31,18 +35,34 @@ class PlayScene extends Phaser.Scene {
 		chem2Button.on('pointerdown', () => { chem2ButtonDown = true;	}, this);
 		chem2Button.on('pointerout', () => { chem2ButtonDown = false; }, this);
 		chem2Button.on('pointerup', () => { chem2ButtonDown = false; }, this);
+		chem3Button.setInteractive({ useHandCursor: true});
+		chem3Button.on('pointerdown', () => { chem3ButtonDown = true;	}, this);
+		chem3Button.on('pointerout', () => { chem3ButtonDown = false; }, this);
+		chem3Button.on('pointerup', () => { chem3ButtonDown = false; }, this);
+		chem4Button.setInteractive({ useHandCursor: true});
+		chem4Button.on('pointerdown', () => { chem4ButtonDown = true;	}, this);
+		chem4Button.on('pointerout', () => { chem4ButtonDown = false; }, this);
+		chem4Button.on('pointerup', () => { chem4ButtonDown = false; }, this);
 
 		this.time.addEvent({
-			delay: 50,
+			delay: 100,
 			callback: () => {
 
 				if (chem1ButtonDown) {
-					let cell = world.grid[0][8];
-					cell.type = 1;
+					let cell = world.grid[0][8 / world.cellSize];
+					cell.type = 11;
 				}
 				if (chem2ButtonDown) {
-					let cell = world.grid[0][24];
-					cell.type = 2;
+					let cell = world.grid[0][24 / world.cellSize];
+					cell.type = 12;
+				}
+				if (chem3ButtonDown) {
+					let cell = world.grid[0][40 / world.cellSize];
+					cell.type = 13;
+				}
+				if (chem4ButtonDown) {
+					let cell = world.grid[0][56 / world.cellSize];
+					cell.type = 14;
 				}
 				this.redrawContents(world, graphics);
 				world.step();
@@ -51,22 +71,74 @@ class PlayScene extends Phaser.Scene {
 			repeat: -1
 		});
 
+		this.sounds = {};
+		this.sounds.music1 = this.sound.add('music1');
+		this.sounds.music2 = this.sound.add('music2');
+
+		//this.sounds.music2.play({ loop: true, volume: 0.6 });
+
+
 	}
 
 	initWorld() {
 
+		const cellSize = 4;
+
 		var world = new CAWorld({
-			width: 64,
-			height: 32,
-			cellSize: 1
+			width: 64 / cellSize,
+			height: 48 / cellSize,
+			cellSize: cellSize
 		});
 
+		/*let colors = {
+			1: 0x333333,
+			// air
+			10: 0xeeeeee,	// airium
+			// base
+			11: 0xffeb3b,	// hydorium
+			12: 0x259b24,	// oxxum
+			13: 0x90a4ae,	// chlorium
+			14: 0x6d4c41,	// sodaium
+			// compounds
+			15: 0x9575cd,	// 3 neutrium
+			16: 0x03a9f4,	// 2 coolium
+			17: 0x81d4fa,	// 1 freezium
+			18: 0xe51c23,	// 5 flamium
+			19: 0xf06292	// 4 feulium
+		};*/
+
+		/*
+		81d4fa	1
+		00bcd4	2
+		03a9f4	3
+		4e6cef	4
+		8e24aa	5
+		f48fb1	6
+		f06292	7
+		f36c60	8
+		e51c23	9
+		*/
+
 		let colors = {
-			0: 0xeeeeee,
-			1: 0x6fedf4,
-			2: 0xf46a45,
-			3: 0xdc96f4
+			1: 0x333333,
+			// air
+			10: 0xeeeeee,	// airium
+			// base
+			11: 0xffeb3b,	// hydorium
+			12: 0x259b24,	// oxxum
+			13: 0x90a4ae,	// chlorium
+			// compounds
+			14: 0x03a9f4,	// 3
+			15: 0x4e6cef,	// 4
+			16: 0x8e24aa,	// 5
+			17: 0xf48fb1,	// 6
+			18: 0xf06292,	// 7
+			19: 0xf36c60,	// 8
+			20: 0x00bcd4,	// 2
+			21: 0x81d4fa,	// 1
+			22: 0xe51c23	// 9
 		};
+
 
 		world.registerCellType('water', {
 			getColor: function() {
@@ -81,50 +153,68 @@ class PlayScene extends Phaser.Scene {
 					if (Math.random() > 0.5) {
 						this.type = left.type;
 						left.type = temp;
+						this.moved = true;
+						left.moved = true;
 					}
 					else {
 						this.type = right.type;
 						right.type = temp;
+						this.moved = true;
+						right.moved = true;
 					}
 				}
 				else if (left && left.type < this.type) {
 					this.type = left.type;
 					left.type = temp;
+					this.moved = true;
+					left.moved = true;
 				}
 				else if (right && right.type < this.type) {
 					this.type = right.type;
 					right.type = temp;
+					this.moved = true;
+					right.moved = true;
 				}
 
 			},
 			reactWithNeighbor: function(left, right) {
 
 				if (left) {
-					if (this.type == 1 && left.type == 2) {
-						this.type = 3;
-						left.type = 3;
+					let reaction = g_game.DEFS.REACTIONS[left.type + '_' + this.type];
+					if (!reaction && reaction !== 0) {
+						reaction = g_game.DEFS.REACTIONS[this.type + '_' + left.type];
 					}
-					else if (this.type == 2 && left.type == 3) {
-						this.type = 3;
-						left.type = 3;
+					if (reaction || reaction === 0) {
+						this.type = reaction;
+						left.type = reaction;
+						this.moved = true;
+						left.moved = true;
 					}
 				}
 				else if (right) {
-					if (this.type == 1 && right.type == 2) {
-						this.type = 3;
-						right.type = 3;
+					let reaction = g_game.DEFS.REACTIONS[right.type + '_' + this.type];
+					if (!reaction && reaction !== 0) {
+						reaction = g_game.DEFS.REACTIONS[this.type + '_' + right.type];
 					}
-					else if (this.type == 2 && right.type == 3) {
-						this.type = 3;
-						right.type = 3;
+					if (reaction || reaction === 0) {
+						this.type = reaction;
+						right.type = reaction;
+						this.moved = true;
+						right.moved = true;
 					}
 				}
 			},
 			process: function(neighbors) {
-				if (this.type === 0) {
-					// already empty
+				if (this.moved) {
+					// already moved or emptyempty
 					return;
 				}
+				//  fly out if lighter than air
+				if (this.type === 1 && !neighbors[world.TOP.index]) {
+					this.type = 10;
+					return;
+				}
+
 				// react with above neighbors
 				this.reactWithNeighbor(neighbors[world.TOP.index]);
 				this.reactWithNeighbor(neighbors[world.TOPLEFT.index], neighbors[world.TOPRIGHT.index]);
@@ -140,14 +230,19 @@ class PlayScene extends Phaser.Scene {
 
 				// push to left or right if still contents
 				this.pushTolighterNeighbor(neighbors[world.LEFT.index], neighbors[world.RIGHT.index]);
+			},
+			reset: function() {
+				this.moved = false;
 			}
 		}, function() {
 			//init
-			this.type = Math.random() < 0 ? 1 : 0;
+			this.type = 10;
+			this.moved = false;
+			/*this.type = Math.random() < 0 ? 10 : 10;
 
 			if (this.type) {
-				this.type = Math.random() < 0 ? 1 : 2;
-			}
+				this.type = Math.random() < 0 ? 10 : 12;
+			}*/
 
 		});
 
